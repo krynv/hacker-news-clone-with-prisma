c4ebd3e5af7ec18d43e162e41abc4b1c27b9851b
VK <vitaliy.krynytskyy@gmail.com>
2019-10-31 08:53:00 +0000

    Moved the resolvers into their own subfolder
    - Implemented the authentication for logging in and loggin out

--- src/index.js
+++ src/index.js
@@ -1,44 +1,27 @@
 const { GraphQLServer } = require('graphql-yoga');
 const { prisma } = require('./generated/prisma-client')
 
-let links = [{
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-}];
-
-let idCount = links.length;
-
-
+const Query = require('./resolvers/Query');
+const Mutation = require('./resolvers/Mutation');
+const User = require('./resolvers/User');
+const Link = require('./resolvers/Link');
 
 const resolvers = {
-    Query: {
-        info: () => null,
-        feed: (root, args, context, info) => {
-            return context.prisma.links()
-        },
-    },
-
-    Link: {
-        id: (parent) => parent.id,
-        description: (parent) => parent.description,
-        url: (parent) => parent.url,
-    },
-
-    Mutation: {
-        post: (root, args, context) => {
-            return context.prisma.createLink({
-                url: args.url,
-                description: args.description,
-            })
-        },
-    },
+    Query,
+    Mutation,
+    User,
+    Link
 };
 
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
     resolvers,
-    context: { prisma },
+    context: request => {
+        return {
+            ...request,
+            prisma,
+        };
+    },
 });
 
 server.start(() => console.log(`Server is running on http://localhost:4000`));
---
+++ src/utils.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+
+const APP_SECRET = '1337c0d3';
+
+function getUserId(context) {
+    const Authorization = context.request.get('Authorization');
+
+    if (Authorization) {
+
+        const token = Authorization.replace('Bearer ', '');
+        const { userId } = jwt.verify(token, APP_SECRET);
+
+        return userId;
+    }
+
+    throw new Error('Not authenticated');
+}
+
+module.exports = {
+    APP_SECRET,
+    getUserId,
+};
